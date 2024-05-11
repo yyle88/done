f58f6ceda90aa6988ab058101478f01d8f55255b
yangyile <example@gogit.com>
2024-05-11 11:17:45 +0700

    换个已经开源的日志(虽然这个日志还是我的)

--- utils_check.go
+++ utils_check.go
@@ -5,32 +5,32 @@ import (
 	"os"
 
 	"github.com/pkg/errors"
-	"gitlab.yyle.com/golang/uvyyle.git/utils_logs/utils_logzap"
+	"github.com/yyle88/zaplog"
 	"go.uber.org/zap"
 )
 
 func Done(err error) {
 	if err != nil {
-		utils_logzap.ZAPS.P1.LOG.Panic("ERROR", zap.Error(errors.WithMessage(err, "wrong")))
+		zaplog.LOGS.P1.Panic("ERROR", zap.Error(errors.WithMessage(err, "wrong")))
 	}
 }
 
 func Must(err error, msg string) {
 	if err != nil {
-		utils_logzap.ZAPS.P1.LOG.Panic("ERROR", zap.Error(errors.WithMessage(err, msg)))
+		zaplog.LOGS.P1.Panic("ERROR", zap.Error(errors.WithMessage(err, msg)))
 	}
 }
 
 func Soft(err error, msg string) {
 	if err != nil {
-		utils_logzap.ZAPS.P1.LOG.Warn("ERROR", zap.Error(errors.WithMessage(err, msg)))
+		zaplog.LOGS.P1.Warn("ERROR", zap.Error(errors.WithMessage(err, msg)))
 	}
 }
 
 // Fata FaFaFaFaFa 哈哈哈哈哈
 func Fata(err error, msg string) {
 	if err != nil {
-		utils_logzap.ZAPS.P1.LOG.Fatal("ERROR", zap.Error(errors.WithMessage(err, msg)))
+		zaplog.LOGS.P1.Fatal("ERROR", zap.Error(errors.WithMessage(err, msg)))
 	}
 }
 
@@ -39,7 +39,7 @@ func EExt(err error, code int) {
 		const format = "\x1b[31;1m%s\x1b[0m" //红色打印
 		var message = fmt.Sprintf(format, errors.WithMessage(err, "wrong"))
 		fmt.Println(message)
-		utils_logzap.ZAPS.P1.LOG.Warn("ERROR", zap.Error(errors.WithMessage(err, "wrong")))
+		zaplog.LOGS.P1.Warn("ERROR", zap.Error(errors.WithMessage(err, "wrong")))
 		os.Exit(code)
 	}
 }
