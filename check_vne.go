a7372874fa47fc88c841f0f8dd0e24f522a04bca
yangyile <example@gogit.com>
2024-05-11 11:32:15 +0700

    增加字符串判断逻辑

---
+++ check_vne.go
@@ -0,0 +1,3 @@
+package done
+
+//TODO 哎不行！还得有一个泛型的 num 类型集合，稍后再弄吧
---
+++ check_vse.go
@@ -0,0 +1,42 @@
+package done
+
+import (
+	"strings"
+
+	"github.com/yyle88/zaplog"
+	"go.uber.org/zap"
+)
+
+type Vse struct {
+	V string
+	E error
+	*Vce[string]
+}
+
+func VSE(val string, err error) *Vse {
+	return &Vse{V: val, E: err, Vce: VCE[string](val, err)}
+}
+
+func (a *Vse) Equals(s string) {
+	if v := a.Done(); v != s {
+		zaplog.LOGS.P1.Panic("wrong", zap.String("v", v), zap.String("s", s))
+	}
+}
+
+func (a *Vse) HasPrefix(prefix string) {
+	if v := a.Done(); !strings.HasPrefix(v, prefix) {
+		zaplog.LOGS.P1.Panic("wrong", zap.String("v", v), zap.String("prefix", prefix))
+	}
+}
+
+func (a *Vse) HasSuffix(suffix string) {
+	if v := a.Done(); !strings.HasSuffix(v, suffix) {
+		zaplog.LOGS.P1.Panic("wrong", zap.String("v", v), zap.String("suffix", suffix))
+	}
+}
+
+func (a *Vse) Contains(sub string) {
+	if v := a.Done(); !strings.Contains(v, sub) {
+		zaplog.LOGS.P1.Panic("wrong", zap.String("v", v), zap.String("sub", sub))
+	}
+}
