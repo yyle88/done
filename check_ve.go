5293f6f7b9e5dd8477b3f35383f32905f78aa2c0
yangyile <example@gogit.com>
2024-08-21 18:48:34 +0700

    添加两个小函数

--- check_ve.go
+++ check_ve.go
@@ -17,7 +17,21 @@ func (a *Ve[V]) Done() V {
 	return a.V //结果不做检查
 }
 
+// 这个类不要定义Must函数，否则会出问题
+//func (a *Ve[V]) Must() V {
+//	Done(a.E)  //只会检查错误，而不检查结果，因此这个类不要定义Must函数，否则会出问题
+//	return a.V //结果不做检查
+//}
+
 func (a *Ve[V]) Soft() V {
 	Soft(a.E)  //有错误时打印告警日志
 	return a.V //结果不做检查
+}
+
+func (a *Ve[V]) Omit() V {
+	return a.V //即使出错也不报错，只把结果返回，结果有可能是正常值也有可能是零值或异常值
+}
+
+func (a *Ve[V]) Skip() V {
+	return a.V //即使出错也不报错，只把结果返回，结果有可能是正常值也有可能是零值或异常值
 }
