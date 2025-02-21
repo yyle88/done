0282faacdb9a8303e78cb972d15e525d92163088
yangyile <gogit@github.com/go-xlan/gogit>
2025-02-21 11:08:50 +0700

    添加函数注释

--- check_func.go
+++ check_func.go
@@ -5,29 +5,32 @@ import (
 	"go.uber.org/zap"
 )
 
-// Done 当err非空是就panic且打印调用栈信息
+// Done panics and logs the stack trace if the error occurs, otherwise do nothing
+// Done 当err非空时就panic且打印调用栈信息
 func Done(err error) {
 	if err != nil {
 		zaplog.LOGS.Skip1.Panic("ERROR", zap.Error(err))
 	}
 }
 
-// Must 当err非空是就panic且打印调用栈信息
+// Must panics and logs the stack trace if the error occurs, otherwise do nothing
+// Must 当err非空时就panic且打印调用栈信息
 func Must(err error) {
 	if err != nil {
 		zaplog.LOGS.Skip1.Panic("ERROR", zap.Error(err))
 	}
 }
 
-// Soft 当err非空是就打印warning日志且打印调用栈信息
+// Soft logs a warning and the stack trace if the error occurs, otherwise do nothing
+// Soft 当err非空时就打印warning日志且打印调用栈信息
 func Soft(err error) {
 	if err != nil {
 		zaplog.LOGS.Skip1.Warn("WARN", zap.Error(err))
 	}
 }
 
+// Fata logs a fatal error and exits the system if the error occurs, otherwise do nothing
 // Fata 当出错时就是调用 log.Fatal 退出系统，当不出错时就不做任何事情
-// FaFaFaFaFa 哈哈哈哈哈
 func Fata(err error) {
 	if err != nil {
 		zaplog.LOGS.Skip1.Fatal("ERROR", zap.Error(err))
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.22.6
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.10.0
-	github.com/yyle88/zaplog v0.0.19
+	github.com/yyle88/zaplog v0.0.20
 	go.uber.org/zap v1.27.0
 )
 
